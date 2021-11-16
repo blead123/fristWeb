89151eeb7ffbf09b9d8c4d8b214257c423317b68
blead123 <blead123@naver.com>
2021-11-17 02:09:20 +0900

    save 구현 성공 ## 실패 사유 ## 한 화면에 구현하려다가 말아먹음 ㅋㅋㅋ post-save머스테치를 만들어서 구현하는것이 맞음

--- src/main/java/com/firstWeb/service/posts/PostsService.java
+++ src/main/java/com/firstWeb/service/posts/PostsService.java
@@ -1,12 +1,16 @@
 package com.firstWeb.service.posts;
 import com.firstWeb.domain.posts.Posts;
 import com.firstWeb.domain.posts.PostsRepository;
+import com.firstWeb.web.dto.PostsListResponseDto;
 import com.firstWeb.web.dto.PostsUpdateRequestDto;
 import com.firstWeb.web.dto.PostsResponseDto;
 import com.firstWeb.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -29,5 +33,11 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다.id="+id));
 
         return new PostsResponseDto(entity);
+    }
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
