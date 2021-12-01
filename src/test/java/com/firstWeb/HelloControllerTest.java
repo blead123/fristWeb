f384b1ac0634dc392e075b5996da130c40b22ea4
blead123 <blead123@naver.com>
2021-12-01 14:11:37 +0900

    테스트 코드 생성

--- src/test/java/com/firstWeb/HelloControllerTest.java
+++ src/test/java/com/firstWeb/HelloControllerTest.java
@@ -1,8 +1,13 @@
 package com.firstWeb;
+import com.firstWeb.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.stereotype.Component;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import com.firstWeb.web.HelloController;
@@ -15,21 +20,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class , excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE ,classes = SecurityConfig.class)
+})
+
+
 
 public class HelloControllerTest {
     @Autowired
     private MockMvc mvc;
 
 
-
+    @WithMockUser(roles = "USER")
     @Test
     public void helloReturned() throws Exception{
         String hello = "hello";
 
         mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
     }
-
+    @WithMockUser(roles = "USER")
     @Test
     public  void helloDtoReturned() throws Exception{
         String name = "hello";
